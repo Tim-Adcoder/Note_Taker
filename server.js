accba9dada38170f5b73f489f51d6c40dde74fd1
TimS <92215345+Tim-Adcoder@users.noreply.github.com>
2022-07-18 20:06:18 +0930

    added server.js file and localhost element

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express'); 
+
+
+
+const PORT = process.env.port || 3001;
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
