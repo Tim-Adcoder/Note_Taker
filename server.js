210e3a83ebe11aabe43033ee8722aac45155b372
TimS <92215345+Tim-Adcoder@users.noreply.github.com>
2022-07-19 16:31:36 +0930

    final add

--- server.js
+++ server.js
@@ -1,33 +1,19 @@
 const express = require('express'); 
-const path = require('path');
-const fs = require('fs');
-// Helper method for generating unique ids
-const uuid = require('./helpers/uuid');
-
-
-const PORT = process.env.port || 3001;
 
 const app = express();
 
+const PORT = process.env.port || 3001;
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-
-// express will handle 
-// hit save it will do a network request from frontend
-// write something handle api/notes request
-//  landing home page with a link to note page
-// db.json refer to persistence class
-//psot request in api/ntoes 
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 
 
